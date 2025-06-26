bde1f489b3bedbcd745414cf3e9c00dce545e16e
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2025-06-27 02:27:39 +0600

    Restart the project...

--- safe_campus/lib/screens/splash_screen.dart
+++ safe_campus/lib/screens/splash_screen.dart
@@ -2,12 +2,29 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => LoginScreen()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    Timer(Duration(seconds: 3), () {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => LoginScreen()));
-    });
-    return Scaffold(body: Center(child: Text('Safe Campus', style: TextStyle(fontSize: 30))));
+    return Scaffold(
+      body: Center(
+        child: Text('Safe Campus', style: TextStyle(fontSize: 30)),
+      ),
+    );
   }
 }
