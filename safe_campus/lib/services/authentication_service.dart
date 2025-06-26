917749dc06105d873691b87efcec9653a31a228b
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2025-06-27 02:36:28 +0600

    Restart the Project...

--- safe_campus/lib/services/authentication_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-class AuthenticationService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  Future<void> signIn(String email, String password) async {
-    await _auth.signInWithEmailAndPassword(email: email, password: password);
-  }
-}
